8bf69e4cdd99ee53c44da83426ba67a48a732978
marko0418 <markonikolic0418@gmail.com>
2021-11-17 11:00:08 +0100

    Added  comment line 4 from repl

--- letsgo.c
+++ letsgo.c
@@ -1,15 +1,14 @@
 
 #incldue <stdio.h>
 
-int main(){
+int main(){/*Another comment from repository 1 (machine 1)*/
 /*Added cimment from repository 1 (machine 1)*/
 printf("Hello World\n");
 printf("added line 1\n");
 printf("Working on a new feature !...");
-<<<<<<< HEAD
+
 /*Added comment from repository 2 (machine 2)*/
-=======
->>>>>>> c8ac6750d2b88d1b6e2808340a8369cdcda9bc93
+
 
 return 0;
 
