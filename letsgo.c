c8ac6750d2b88d1b6e2808340a8369cdcda9bc93
marko0418 <markonikolic0418@gmail.com>
2021-11-17 10:38:39 +0100

    Added comment from rep1

--- letsgo.c
+++ letsgo.c
@@ -2,10 +2,11 @@
 #incldue <stdio.h>
 
 int main(){
-
+/*Added cimment from repository 1 (machine 1)*/
 printf("Hello World\n");
 printf("added line 1\n");
 printf("Working on a new feature !...");
+
 return 0;
 
 
